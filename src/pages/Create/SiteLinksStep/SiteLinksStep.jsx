290d248ae0d4e0af7b8ff7c761b339a42543459c
Paulo Pimentel <fudevil123@gmail.com>
2024-09-22 22:33:02 -0300

    new

--- src/pages/Create/SiteLinksStep/SiteLinksStep.jsx
+++ src/pages/Create/SiteLinksStep/SiteLinksStep.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { loadStripe } from '@stripe/stripe-js';
-import { useMutation } from '@tanstack/react-query';
 import './SiteLinksStep.scss';
 
 const stripePromise = loadStripe('pk_test_51Q1x2cRraDIE2N6qbyls0V3OWLG43f6fV0O5rLdgZjyBQrcXTubZmvoxBX7DiPLmFHxBjOGsBWrJeb73jPYJftKO006qSKveLt');
@@ -10,33 +9,48 @@ const SiteLinksStep = ({ setStep, handleSubmit }) => {
   const { t } = useTranslation();
   const [submitted, setSubmitted] = useState(false);
 
-  const createCheckoutSession = async () => {
-    const response = await fetch('https://promote-site-back.vercel.app/api/create-checkout-session', {
+  const handleStripePayment = async () => {
+    const response = await fetch('/api/create-checkout-session', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
-    return response.json();
-  };
 
-  const mutation = useMutation(createCheckoutSession, {
-    onSuccess: async (data) => { // Torne a função assíncrona
-      const stripe = await stripePromise; // Use await aqui
-      const { error } = await stripe.redirectToCheckout({ sessionId: data.id });
-      if (error) {
-        console.error('Stripe Error:', error);
-      }
-    },
-    onError: (error) => {
-      console.error('Erro ao criar sessão de checkout:', error);
-    },
-  });
+    if (!response.ok) {
+      console.error('Erro ao criar sessão de checkout:', response.statusText);
+      return false; // Indica que o pagamento falhou
+    }
+
+    const data = await response.json();
+    const stripe = await stripePromise;
+
+    const { error } = await stripe.redirectToCheckout({
+      sessionId: data.id,
+    });
+
+    if (error) {
+      console.error('Stripe Error:', error);
+      return false; // Indica que o pagamento falhou
+    }
+
+    return true; // Indica que o pagamento foi realizado com sucesso
+  };
 
   const handleSubmitWithValidation = async (e) => {
     e.preventDefault();
     setSubmitted(true);
 
     // Inicia o pagamento
-    mutation.mutate();
+    const paymentApproved = await handleStripePayment();
+    if (paymentApproved) {
+      handleSubmit(e, true); // Envia com hot: 1
+    } else {
+      console.log('Pagamento não aprovado');
+    }
+  };
+
+  const handleNormalSubmit = (e) => {
+    e.preventDefault();
+    handleSubmit(e, false); // Envia com hot: 0
   };
 
   return (
@@ -56,7 +70,7 @@ const SiteLinksStep = ({ setStep, handleSubmit }) => {
         <button onClick={() => setStep((prev) => prev - 1)} type='button' className="back-button">
           {t('agree.back')}
         </button>
-        <button onClick={() => handleSubmit(e, false)} type='button'>
+        <button onClick={handleNormalSubmit} type='button'>
           {t('agree.submit')}
         </button>
       </div>
